7708d34d2b01f900594ec6ffdde25e115daeaed5
Kevin Harry <kevinharrywork@gmail.com>
2026-02-11 10:01:59 +0530

    Time: 75 ms (50.26%), Space: 47 MB (32.11%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        String a =s+s;
+        String b=a.substring(1,a.length()-1);
+        return b.contains(s);
+        
+    }
+}
