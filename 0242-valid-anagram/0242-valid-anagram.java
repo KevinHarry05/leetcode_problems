9442fec0ef90515c421650d0452249607ccf8809
Kevin Harry <kevinharrywork@gmail.com>
2026-03-05 18:19:53 +0530

    Time: 3 ms (91.83%), Space: 46 MB (49.56%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,15 +1,12 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()-1 !=t.length()-1)
-        {
-            return false;
-        }
-        char[] a=s.toCharArray();
-        char[] b=t.toCharArray();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        return Arrays.equals(a,b);
-
+        if(s.length()-1 != t.length()-1 ) return false;
+        char[] arr1=s.toCharArray();
+        char[] arr2=t.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        return Arrays.equals(arr1,arr2);
+        
         
     }
 }
