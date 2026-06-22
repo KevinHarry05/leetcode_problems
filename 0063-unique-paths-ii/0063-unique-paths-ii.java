4784f429c1e2295344441ac3897eefbc4dce1c9a
Kevin Harry <kevinharrywork@gmail.com>
2026-06-22 09:10:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+     public int solve( int i , int j ,int[][] grid, Integer[][] dp){
+        int m = grid.length;
+        int n = grid[0].length;
+            if(i>=m || j>=n){
+                return 0;
+            }
+            if(i==m-1 && j==n-1){
+                return 1;
+            }
+            if(grid[i][j]==1){
+                return 0;
+            }
+            // if(dp[i][j]!=null){
+            //     return dp[i][j];
+            // }
+            dp[i][j]= solve(i+1,j,grid,dp) + solve(i,j+1,grid,dp);
+            return dp[i][j];
+        }
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+       
+        int m= obstacleGrid.length;
+        int n= obstacleGrid[0].length;
+        Integer[][] dp= new Integer[m][n];
+        return solve(0,0,obstacleGrid,dp);
+        
+    }
+}
