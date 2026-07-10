46e4b02aac824af9c857fdb7585fcedce141fe60
Kevin Harry <kevinharrywork@gmail.com>
2026-07-10 19:26:56 +0530

    Time: 353 ms (90.17%), Space: 0B (100%) - LeetHub

---
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -0,0 +1,5 @@
+SELECT query_name,
+    round(avg(rating/position),2) as quality , round(avg(rating < 3)*100,2) as poor_query_percentage
+FROM Queries
+WHERE query_name IS NOT NULL
+GROUP BY query_name;
