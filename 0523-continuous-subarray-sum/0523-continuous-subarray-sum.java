af804800b92e3ed65c08fa137219835b542eb593
Kevin Harry <kevinharrywork@gmail.com>
2026-07-15 21:44:01 +0530

    Time: 24 ms (63.41%), Space: 139.7 MB (65.05%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -1,29 +1,12 @@
-import java.util.HashMap;
-
 class Solution {
-
     public boolean checkSubarraySum(int[] nums, int k) {
-
         HashMap<Integer, Integer> map = new HashMap<>();
-
-        // remainder 0 occurs before the array starts
         map.put(0, -1);
-
         int prefixSum = 0;
-
         for (int i = 0; i < nums.length; i++) {
-
             prefixSum += nums[i];
-
             int remainder = prefixSum % k;
-
-            // Handle negative remainders
-            if (remainder < 0) {
-                remainder += k;
-            }
-
             if (map.containsKey(remainder)) {
-
                 if (i - map.get(remainder) >= 2) {
                     return true;
                 }
@@ -31,7 +14,6 @@ class Solution {
                 map.put(remainder, i);
             }
         }
-
         return false;
     }
 }
