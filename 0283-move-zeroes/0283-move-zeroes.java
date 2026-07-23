9e551d4b34fb2d628b9d1b5eb1d0376ccab38e45
Kevin Harry <kevinharrywork@gmail.com>
2026-07-23 22:33:24 +0530

    Time: 2 ms (92.13%), Space: 47.8 MB (38.33%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -6,10 +6,10 @@ class Solution {
             if(nums[i]!=0)
             {
                 int temp=nums[i];
-                nums[i]=nums[j];nums[j]=temp;
+                nums[i]=nums[j];
+                nums[j]=temp;
                 j++;
             }
-        }
-        
+        }        
     }
 }
