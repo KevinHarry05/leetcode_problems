decb822499a60b4f14576066db8791c3c068f084
Kevin Harry <kevinharrywork@gmail.com>
2026-09-08 18:12:06 +0530

    Time: 478 ms (14.11%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,6 +1,6 @@
-select m.name
-from Employee e
-join Employee m
-on m.id=e.managerId
-group by m.id,m.name
-having count(*) >= 5;
+SELECT e1.name
+FROM Employee e1
+INNER JOIN Employee e2
+ON e1.id=e2.managerid
+GROUP BY e2.managerid
+HAVING COUNT(e2.managerid)>=5
